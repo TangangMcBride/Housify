1238eb41a6ce66f201c7c2f9196d23a80fbfa387
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2025-03-31 23:41:26 +0100

    Add Home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './scss/layout.scss'
-import Navbar from './components/Navbar.jsx'
-
+import Navbar from './components/Navbar/Navbar.jsx'
+import Home from './pages/Home.jsx'
 function App() {
  
 
   return (
     <div className='layout'>
       <Navbar />
+      <div className='content'>
+        <Home/>
+      </div>
     </div>
   )
 }
