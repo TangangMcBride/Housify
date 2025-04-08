e3ce6ab3325b8d171a6352141e45193a31c7a66c
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2025-04-08 07:42:48 +0100

    Add Map components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './scss/layout.scss'
 import Navbar from './components/Navbar/Navbar.jsx'
-import Home from './pages/homePage/Home.jsx'
 import { Outlet } from 'react-router-dom'
 function App() {
  
